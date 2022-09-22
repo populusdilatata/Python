2c3dfe71fb1c9a551e652937b3a1c0c97f21a9d3
populusdilatata <tom.majda@seznam.cz>
2022-09-22 14:25:36 +0200

    Add files via upload
    NumPy

---
+++ numpy examples/A-Basic/AA-GettingStarted/AA-3VersionofNP.py
@@ -0,0 +1,5 @@
+#Checking NumPy Version
+#The version string is stored under __version__ attribute.
+import numpy as np
+
+print(np.__version__)
---
+++ numpy examples/A-Basic/AB-CretingArrays/AB-1CreatingArrays.py
@@ -0,0 +1,10 @@
+#NumPy is used to work with arrays. The array object in NumPy is called ndarray.
+#We can create a NumPy ndarray object by using the array() function.
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+#type(): This built-in Python function tells us the type of the object
+#passed to it. Like in above code it shows that arr is numpy.ndarray type.
+print(type(arr))
