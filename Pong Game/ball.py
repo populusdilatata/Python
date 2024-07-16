8fc6c800ed11f82d21bb950caddd15f114bbd561
populusdilatata <tom.majda@seznam.cz>
2024-07-17 01:12:36 +0200

    Add files via upload
    Commented version of main part of Pong Game

--- Pong Game/ball.py
+++ Pong Game/ball.py
@@ -5,23 +5,26 @@ class Ball(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.color("white")
         self.shape("circle")
+        self.color("white")
         self.penup()
+        self.move_speed = 0.5
         self.x_move = 3
         self.y_move = 3
-        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+        # self.setheading(45)
+        # self.forward(2)
 
     def bounce_y(self):
-        self.y_move *= -1
+        self.y_move *=-1
 
     def bounce_x(self):
-        self.x_move *= -1
+        self.x_move *=-1
+        #Reducing the time pause will increase the speed of game
         self.move_speed *= 0.9
 
     def reset_position(self):
